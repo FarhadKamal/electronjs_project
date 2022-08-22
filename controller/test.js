6fea1533a85d2a859af5ec521554da5b5ea5e7de
Shahriar1924 <shopbottolabazar2020@gmail.com>
2022-08-22 10:24:19 +0600

    Shahriar First Commit

--- controller/test.js
+++ controller/test.js
@@ -4,8 +4,9 @@ const { getConnectionString, dblost } = require("../db");
 let subWindow = null;
 let trackData;
 let trackUnitData;
+let trackRawMaterialsData;
 
-const createSemiGoodWindow = () => {
+const createTestWindow = () => {
   if (!subWindow) {
     subWindow = new BrowserWindow({
       width: 1200,
@@ -18,7 +19,7 @@ const createSemiGoodWindow = () => {
       },
     });
 
-    subWindow.loadFile("view/semi_good/list.html");
+    subWindow.loadFile("view/test/list.html");
 
     subWindow.on("closed", function () {
       subWindow = null;
@@ -28,7 +29,7 @@ const createSemiGoodWindow = () => {
   }
 };
 
-ipcMain.on("semi:good:delete:request", function (e, id) {
+ipcMain.on("test:delete:request", function (e, id) {
   let response = dialog.showMessageBoxSync(subWindow, {
     type: "question",
     buttons: ["Yes", "No"],
@@ -37,49 +38,50 @@ ipcMain.on("semi:good:delete:request", function (e, id) {
   });
   if (response == 0) {
     deleteQuery(id, function (reply) {
-      if (reply == "success") subWindow.loadFile("view/semi_good/list.html");
-      else e.reply("semi:good:list:deleted", reply);
+      if (reply == "success") subWindow.loadFile("view/test/list.html");
+      else e.reply("test:list:deleted", reply);
     });
   }
 });
 
-ipcMain.on("semi:good:edit:request", function (e, id) {
+ipcMain.on("test:edit:request", function (e, id) {
   get_by_id(id, function (row) {
     trackData = row;
-    subWindow.loadFile("view/semi_good/edit.html");
+    subWindow.loadFile("view/test/edit.html");
   });
 });
 
-ipcMain.on("semi:good:add:loaded", function (e, id) {
-  e.reply("semi:good:add:fetched", trackUnitData);
+ipcMain.on("test:add:loaded", function (e, id) {
+  e.reply("test:add:fetched", trackUnitData);
+  e.reply("test:add:fetched", trackRawMaterialsData);
 });
 
-ipcMain.on("semi:good:edit:loaded", function (e, id) {
-  e.reply("semi:good:edit:fetched", [trackData, trackUnitData]);
+ipcMain.on("test:edit:loaded", function (e, id) {
+  e.reply("test:edit:fetched", [trackData, trackUnitData, trackRawMaterialsData]);
 });
 
-ipcMain.on("semi:good:add:submit", function (e, item) {
-  console.log(item);
-  // insert(item, function (reply) {
-  //   e.reply("semi:good:add:reply", reply);
-  // });
+ipcMain.on("test:add:submit", function (e, item) {
+  // console.log(item);
+  insert(item, function (reply) {
+    e.reply("test:add:reply", reply);
+  });
 });
 
-ipcMain.on("semi:good:edit:submit", function (e, item) {
+ipcMain.on("test:edit:submit", function (e, item) {
   update(item, function (reply) {
-    e.reply("semi:good:edit:reply", reply);
+    e.reply("test:edit:reply", reply);
   });
 });
 
-ipcMain.on("semi:good:add:click", function (e, item) {
-  subWindow.loadFile("view/semi_good/add.html");
+ipcMain.on("test:add:click", function (e, item) {
+  subWindow.loadFile("view/test/add.html");
 });
 
-ipcMain.on("semi:good:list:click", function (e, item) {
-  subWindow.loadFile("view/semi_good/list.html");
+ipcMain.on("test:list:click", function (e, item) {
+  subWindow.loadFile("view/test/list.html");
 });
 
-ipcMain.on("semi:good:list:loaded", function (e, item) {
+ipcMain.on("test:list:loaded", function (e, item) {
   let html = "";
 
   get_list(function (rows) {
@@ -90,21 +92,21 @@ ipcMain.on("semi:good:list:loaded", function (e, item) {
       html += sl;
       html += "</td>";
       html += "<td>";
-      html += row.semi_good_name;
+      html += row.test_name;
       html += "</td>";
       html += "<td>";
-      html += row.semi_good_unit;
+      html += row.unit_name;
       html += "</td>";
 
       html += "<td>";
       html +=
         '<button type="button" class="btn btn-danger btn-sm rounded-0" onClick="deleteMe(\'' +
-        row.semi_good_id +
+        row.id +
         "')\" ><i class='material-icons'>&#xE872;</i></button> &nbsp;";
 
       html +=
         '<button type="button" class="btn btn-success btn-sm rounded-0" onClick="editMe(\'' +
-        row.semi_good_id +
+        row.id +
         "')\" ><i class='material-icons'>&#xE254;</i></button>";
       html += "</td>";
 
@@ -112,16 +114,25 @@ ipcMain.on("semi:good:list:loaded", function (e, item) {
       sl++;
     });
 
-    e.reply("semi:good:list:table", html);
+    e.reply("test:list:table", html);
   });
+   
   get_units(function (rows) {
     trackUnitData = rows;
   });
+
+  get_raw_materials(function (rows) {
+    trackRawMaterialsData = rows;
+  });
+
+  
 });
 
 function insert(item, callback) {
   const connection = mysql.createConnection(getConnectionString());
-  console.log(item);
+
+  console.log(item[0]);
+  console.log(item[1]);
   // connect to mysql
   connection.connect(function (err) {
     // in case of error
@@ -135,16 +146,17 @@ function insert(item, callback) {
 
   //data cleaning
 
-  let seminame = item[0].replace(/['"]+/g, "");
+  let testname = item[0].replace(/['"]+/g, "");
+  let testUnit = item[1].replace(/['"]+/g, "");
 
   // Perform a query
 
   query =
-    "INSERT INTO semi_good_list (semi_good_name, semi_good_unit) VALUES \
+    "INSERT INTO test (test_name, unit_name) VALUES \
    ('" +
-    seminame +
+   testname +
     "', '" +
-    item[1] +
+    testUnit +
     "')";
 
   connection.query(query, function (err, result) {
@@ -185,16 +197,16 @@ function update(item, callback) {
   // Perform a query
 
   query =
-    "update  semi_good_list \
+    "update  test \
     set \
-    semi_good_name = '" +
+    test_name = '" +
     matname +
     "' , \
-    semi_good_unit = '" +
+    unit_name = '" +
     item[2] +
     "'\
     where\
-    semi_good_id = '" +
+    test.id = '" +
     item[0] +
     "' ";
 
@@ -231,7 +243,7 @@ function deleteQuery(id, callback) {
 
   // Perform a query
 
-  query = "delete from semi_good_list where semi_good_id=" + id;
+  query = "delete from test where test.id=" + id;
 
   connection.query(query, function (err, result) {
     if (err) {
@@ -265,7 +277,7 @@ function get_list(callback) {
   });
 
   // Perform a query
-  query = "select * from semi_good_list order by semi_good_name";
+  query = "select * from test order by test.id";
 
   connection.query(query, function (err, rows, fields) {
     if (err) {
@@ -298,7 +310,7 @@ function get_by_id(id, callback) {
   });
 
   // Perform a query
-  query = "select * from semi_good_list where semi_good_id=" + id;
+  query = "select * from test where test.id=" + id;
 
   connection.query(query, function (err, rows, fields) {
     if (err) {
@@ -347,4 +359,37 @@ function get_units(callback) {
   });
 }
 
-module.exports = { createSemiGoodWindow };
+
+function get_raw_materials(callback) {
+  const connection = mysql.createConnection(getConnectionString());
+
+  // connect to mysql
+  connection.connect(function (err) {
+    // in case of error
+    if (err) {
+      console.log(err.code);
+      console.log(err.fatal);
+
+      dblost();
+    }
+  });
+
+  // Perform a query
+  query = "Select * from raw_material_list ORDER BY raw_material_list.raw_mat_name";
+  // console.log(query);
+  connection.query(query, function (err, rows, fields) {
+    if (err) {
+      console.log("An error ocurred performing the query.");
+      console.log(err);
+      return;
+    }
+    callback(rows);
+  });
+
+  // Close the connection
+  connection.end(function () {
+    // The connection has been closed
+  });
+}
+
+module.exports = { createTestWindow };
